c9ee6456a58c62530072c32f72a479ec4fd060ce
William Zhang <willzhang05@gmail.com>
2018-02-24 22:51:10 -0500

    add new logging script

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from flask import Flask, Response, render_template
 
 app = Flask(__name__)
 
+#buff = [None] * 188
 buff = None
+index = 0
 
 @app.before_first_request
 def connect():
@@ -19,8 +21,17 @@ def connect():
         mreq = struct.pack("=4sl", socket.inet_aton(anycast_addr), socket.INADDR_ANY)
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
         global buff
+        global index
         while True:
-            buff = sock.recv(10240)
+            recv = sock.recv(10240)
+            buff = recv
+            #index = buff.index(b'G')
+            #recv = recv[index:index + 188]
+            #s_recv = [recv[i:i+2] for i in range(0, len(recv), 2)]
+            #for i in range(len(s_recv)):
+            #    buff[i] = (int(s_recv[i], base=16))[2:]
+            #    print(buff[i])
+            
     thread = threading.Thread(target=recv_native, args=[anycast_addr, anycast_port])
     thread.start()
 
@@ -30,6 +41,7 @@ def index():
 
 @app.route('/video')
 def video():
+    print(buff)
     return Response(buff, mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
---
+++ logs/new_log.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+import glob, os
+import re
+import ipwhois
+
+def main():
+    unique = dict()
+    #unique = set()
+    for filename in glob.glob("*.log"):
+        with open(filename, 'r') as f:
+            line = None
+            while line != '':
+                line = f.readline().strip()
+                attr = line.split('\t')
+                new_attr = []
+                for a in attr:
+                    sub = re.sub(r'[A-z/]+:\s', '', a).strip()
+                    new_attr.append(sub)
+                if len(new_attr) > 1:
+                    new_attr[1] = re.sub(r'/\d{2,4}', '', new_attr[1])
+                    if new_attr[1] in unique:
+                        unique[new_attr[1]] = (new_attr[0], max(int(unique[new_attr[1]][1]), int(new_attr[3])))
+                    else:
+                        unique[new_attr[1]] = (new_attr[0], int(new_attr[3]))
+                #unique[attr[1]] = 
+                #if attr[0] != '':
+                #    new_attr = attr[0:2]
+                #    new_attr.extend([attr[3]])
+                #    unique.add('\t'.join(new_attr))
+
+    #out = sorted(list(unique))
+    keys = sorted(unique.keys())
+    for k in keys:
+        whois = ipwhois.IPWhois(k)
+        info = whois.lookup_rdap()
+        asn_desc = info['asn_description']
+        desc = None
+        if info['network']['remarks'] is not None:
+            desc = info['network']['remarks'][0]['description']
+
+        print()
+        print('Source: ', k, '\t\t\tGroup: ', unique[k][0], '\t\t\tPPS: ', unique[k][1])
+        if asn_desc is not None:
+            print(asn_desc)
+        #else:
+        #    print(info)
+        if desc is not None:
+            print(desc)
+
+
+if __name__ == "__main__":
+    main()
